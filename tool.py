7953db347fb748c2275cff388cd7d8249c8ed359
Al Johri <al.johri@gmail.com>
2014-05-16 13:29:45 -0500

    Add socialscraper as gitsubmodule.

--- tool.py
+++ tool.py
@@ -1,7 +1,11 @@
 import os
+import environment
+
 from facebook import GraphAPI, GraphAPIError
-from socialscraper.facebook.facebook.graphapi.feed import get_feed
+from socialscraper.facebook.graphapi import get_feed
 
 FACEBOOK_USER_TOKEN = os.getenv('FACEBOOK_USER_TOKEN')
 
 api = GraphAPI(FACEBOOK_USER_TOKEN)
+
+get_feed(api, '357858834261047')
