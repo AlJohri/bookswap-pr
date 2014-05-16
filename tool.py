f32b2e06791c72dace41be5e0301b47dbe013556
Al Johri <al.johri@gmail.com>
2014-05-16 12:29:09 -0500

    Added .secret.example for graph api token.

--- tool.py
+++ tool.py
@@ -1,1 +1,7 @@
+import os
+from facebook import GraphAPI, GraphAPIError
 from socialscraper.facebook.facebook.graphapi.feed import get_feed
+
+FACEBOOK_USER_TOKEN = os.getenv('FACEBOOK_USER_TOKEN')
+
+api = GraphAPI(FACEBOOK_USER_TOKEN)
